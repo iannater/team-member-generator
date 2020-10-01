ac077b2e7b7f16a4ceba9bf8c4de63a503b7f39d
Ian Nater <ian.nater@gmail.com>
2020-10-01 11:01:11 -0500

    I am victoriouous! In this push I completed the switch inside of the create team function and completed the build team function to make the file.

--- Develop/app.js
+++ Develop/app.js
@@ -11,9 +11,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 const team = [];
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// This is my array of questions which I call inside of each respective function below. 
 const managerQuestions = [
     {
         type: "input",
@@ -40,31 +38,51 @@ const managerQuestions = [
     },
 ];
 
-const employeeQuestions = [
+const engineerQuestions = [
     {
         type: "input",
-        name: "employeeName",
-        message: "Please enter the employees name",
+        name: "engineerName",
+        message: "Please enter the Engineer's Name",
     },
+    {
+        type: "input",
+        name: "engineerID",
+        message: "Please enter the Engineer's work ID Number",
+    },
+    {
+        type: "input",
+        name: "engineerEmail",
+        message: "Please enter the Engineer's work email address",
+    },
+    {
+        type: "input",
+        name: "engineerGithub",
+        message: "Please enter the Engineer's GitHub username",
+    },
+];
 
+const internQuestions = [
     {
         type: "input",
-        name: "managerID",
-        message: "Please enter the employees work ID number",
+        name: "internName",
+        message: "Please enter the intern's Name",
     },
-
     {
         type: "input",
-        name: "managerEmail",
-        message: "Please enter the employees work email address",
+        name: "internID",
+        message: "Please enter the intern's work ID Number",
     },
-
     {
         type: "input",
-        name: "managerOfficeNumber",
-        message: "Please enter the managers office number",
+        name: "internEmail",
+        message: "Please enter the intern's work email address",
     },
-]
+    {
+        type: "input",
+        name: "internSchool",
+        message: "Please enter the intern's school name",
+    },
+];
 
 const teamQuestions = [{
     type: "list",
@@ -73,61 +91,72 @@ const teamQuestions = [{
     choices: ["intern", "engineer", "I am done adding team members"]
 }]
 
+// This is where my functions start. The first function youll see is the managerInfo(). This prompts the user with the manager questions. At the end of that function I call the create team function. This function asks the user which employees they want to create and then based on their selections calls a function for that employee which prompts them with those employees questions (intern/engineer). Once done they select I am done and this calls the build team function which makes the files.
+function managerInfo() {
+    inquirer.prompt(managerQuestions)
+        .then(responses => {
+
+            const manager = new Manager(responses.managerName, responses.managerID, responses.managerEmail, responses.managerOfficeNumber)
+            console.log("New manager", manager)
+            team.push(manager);
+            createTeam();
+        })
+};
+
+managerInfo();
+
+// Function that asks the Engineer questions. This comes after the manager question and is called in the switch inside createTeam(). 
+function engineerFunction() {
+    inquirer.prompt(engineerQuestions)
+        .then(responses => {
+            // take their answers and build the new Engineer class object
+            const engineer = new Engineer(responses.engineerName, responses.engineerID, responses.engineerEmail, responses.engineerGithub)
+            console.log("New engineer", engineer)
+            // Push that object into the team array
+            team.push(engineer);
+            createTeam();
+        })
+}
+// Function that asks the Intern questions. This comes after the manager question and is called in the switch inside createTeam(). 
+function internFunction() {
+    inquirer.prompt(internQuestions)
+        .then(responses => {
+            // take their answers and build the new Intern class object
+            const intern = new Intern(responses.internName, responses.internID, responses.internEmail, responses.internSchool)
+            console.log("New intern", intern)
+            // Push that object into the team array
+            team.push(intern);
+            createTeam();
+        })
+}
 
 
-function createTeam(){
-        inquire.prompt(teamQuestions)
+function createTeam() {
+    inquirer.prompt(teamQuestions)
         .then(teamRes => {
-          
+            // Switch to ask the user what employees they want to create.
             switch (teamRes.employeeType) {
-                case "engineer": 
-                    //call fucntion to ask enginner questions
+                // If they want to create an engineer then we call the engineer function which prompts them with the engineer questions
+                case "engineer":
+                    engineerFunction();
                     break;
+                    // If they want to create an intern then we call the intern function which prompts them with the intern questions
                 case "intern":
-                //to ask intern question internInfo();
+                    internFunction();
                     break;
+                    // If they are done selecting employees then we call the build team function below and generate the file with their answers.
                 default:
-                /// function will help build our team
+                    buildTeam();
                     break;
             }
         })
 };
 
-
-
-function managerInfo(){
-    inquirer.prompt(managerQuestions)
-    .then(responses => {
-       
-        const manager = new Manager(responses.managerName, responses.managerID, responses.managerEmail, responses.managerOfficeNumber)
-        console.log("New manager", manager)
-        team.push(manager);
-        createTeam();
-        //call a function thats going to ask if you want to 
-        // - create and enginer
-        //- create intern
-        //- stop makinga team
-    })
+// This function builds the file once all employees have been entered by the user using the createTeam(). It is the last function inside of the switch.  
+function buildTeam(){
+    // Render the file if output doesnt exist already
+    fs.existsSync(OUTPUT_DIR) || fs.mkdirSync(OUTPUT_DIR);
+    //This writes the file to the output folder using htmlrenderer.js
+    fs.writeFileSync(outputPath, render(team));       
 };
 
-managerInfo();
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
